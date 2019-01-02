dda527589ab8665a01d6dbea5b33bacc3d811159
Sonali Hiranandani <hiranandanisonal@gmail.com>
2019-01-03 00:11:19 +0530

    Candidate list

--- contracts/election.sol
+++ contracts/election.sol
@@ -1,11 +1,25 @@
 pragma solidity ^0.5.0;
 contract Election
 {
-    string public candidate;
-    // constructor
     constructor() public
     {
-        candidate = "candidate 1";
+        addCandidate("Sonali");
+        addCandidate("Sahil");
+
+    }
+    struct Candidate
+    {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    mapping(uint => Candidate) public candidates;
+    uint public candidatesCount;
+    
+    function addCandidate(string memory m_name) private{
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, m_name, 0);
 
     }
 }
