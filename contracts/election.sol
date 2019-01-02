e52bcb367101a24149588093ae2d1094cfe8ad15
Sonali Hiranandani <hiranandanisonal@gmail.com>
2019-01-03 02:24:10 +0530

    Cast the vote

--- contracts/election.sol
+++ contracts/election.sol
@@ -13,6 +13,8 @@ contract Election
         string name;
         uint voteCount;
     }
+    // store accounts that have voted
+    mapping(address => bool) public voters;
 
     mapping(uint => Candidate) public candidates;
     uint public candidatesCount;
@@ -21,5 +23,18 @@ contract Election
         candidatesCount++;
         candidates[candidatesCount] = Candidate(candidatesCount, m_name, 0);
 
+    }
+
+    function vote(uint _candidateId) public{
+        //require that they havent voted before
+        require(!voters[msg.sender]);
+
+        //require a valid candidate
+        require(_candidateId>0 && _candidateId <= candidatesCount);
+
+        //record that the voter has voted
+        voters[msg.sender] = true;
+        //update candidate vote count
+        candidates[_candidateId].voteCount++;
     }
 }
